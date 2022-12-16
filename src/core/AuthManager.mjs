eae633a59c1eeed19de6153e9a2f095bc0a43198
johnSamilin <temp.kroogi@gmail.com>
2022-12-16 10:41:27 +0300

    Adds auto-login flow

--- src/core/AuthManager.mjs
+++ src/core/AuthManager.mjs
@@ -1,5 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js'
+import { getAuth, signOut, GoogleAuthProvider, signInWithPopup, setPersistence, browserSessionPersistence } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js'
+import { SettingsManager } from './SettingsManager.mjs';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCpPz_8duNVNfVhWp81BV6HHORussPUPLg",
@@ -47,26 +48,33 @@ class CAuthManager {
     }
 
     start = () => {
-        this.#gApp = initializeApp(firebaseConfig)
-        this.#gAuth = getAuth(this.#gApp)
-        this.#gProvider = new GoogleAuthProvider()
+        return new Promise((resolve) => {
+            this.#gApp = initializeApp(firebaseConfig)
+            this.#gAuth = getAuth(this.#gApp)
+            if (SettingsManager.autoLoginEnabled) {
+                this.#gAuth.onAuthStateChanged(() => {
+                    if (this.#gAuth.currentUser?.uid) {
+                        this.isLoggedIn = true
+                    }
+                    resolve()
+                })
+            } else {
+                resolve()
+            }
+        })
     }
 
     login = async () => {
         try {
+            this.#gProvider = new GoogleAuthProvider()
+            await setPersistence(this.#gAuth, browserSessionPersistence)
             const result = await signInWithPopup(this.#gAuth, this.#gProvider)
             const credential = GoogleAuthProvider.credentialFromResult(result)
             const token = credential.accessToken
             const user = result.user
             this.isLoggedIn = true
-        } catch(error) {
+        } catch (error) {
             console.error(error)
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
         }
     }
 
@@ -74,6 +82,7 @@ class CAuthManager {
         this.onLogout()
         this.isLoggedIn = false
         this.#data = null
+        signOut(this.#gAuth)
     }
 
 }
